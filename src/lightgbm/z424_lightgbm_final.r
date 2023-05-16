46672c11e773821016ca6abca23ba5c3d5a13d0b
manumaxg <104378269+manumaxg@users.noreply.github.com>
2023-05-16 14:57:51 -0300

    k

--- src/lightgbm/z424_lightgbm_final.r
+++ src/lightgbm/z424_lightgbm_final.r
@@ -83,6 +83,7 @@ modelo  <- lgb.train( data= dtrain,
                                    num_leaves=         PARAM$finalmodel$num_leaves,
                                    min_data_in_leaf=   PARAM$finalmodel$min_data_in_leaf,
                                    feature_fraction=   PARAM$finalmodel$feature_fraction,
+                                   
                                    seed=               PARAM$finalmodel$semilla
                                   )
                     )
