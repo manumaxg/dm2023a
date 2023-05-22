f76d7149fe30a4167a26b8fa8db31023ca9cc19e
manumaxg <104378269+manumaxg@users.noreply.github.com>
2023-05-22 20:18:24 -0300

    d

--- src/workflow-colab/651_HT_lightgbm.r
+++ src/workflow-colab/651_HT_lightgbm.r
@@ -31,9 +31,9 @@ options(error = function() {
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento  <- "HT6513"
+PARAM$experimento  <- "HT6514"
 
-PARAM$exp_input  <- "TS6413"
+PARAM$exp_input  <- "TS6414"
 
 PARAM$lgb_crossvalidation_folds  <- 5  #En caso que se haga cross validation, se usa esta cantidad de folds
 
