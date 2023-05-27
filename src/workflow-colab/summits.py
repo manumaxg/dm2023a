e9437ec676242fa91d2a0dc6367403b7c80443e7
manumaxg <104378269+manumaxg@users.noreply.github.com>
2023-05-26 23:13:12 -0300

    k

--- src/workflow-colab/summits.py
+++ src/workflow-colab/summits.py
@@ -3,25 +3,28 @@ import pandas as pd
 import subprocess
 # fijar directorio de trabajo
 os.environ['KAGGLE_USERNAME'] = 'experimentos2'
-os.environ['KAGGLE_KEY'] = "5a354fcb63b1264dc81c48039b104fa9"
-os.chdir("C:/Users/mmgma/Downloads/Experimento14/ZZ6914")
-semillas  = ( 158771, 616523, 742499, 217981, 235439, 355537, 357755, 775733, 337753,
-                       575533 )
+os.environ['KAGGLE_KEY'] = "41233daee824c4ac25c2bcc1d3e031ad"
+os.chdir("C:/Users/mmgma/Downloads/ZZ6913")
+semillas  =  (158771, 616523, 742499, 217981, 235439) 
+#gsutil -m cp -r gs://mia99/exp/ZZ6916    C:/Users/mmgma/Downloads/
+
+
+
 # nombres de archivos del directorio
 print(os.listdir())
 # Define variables
-semilla = 235439
-experimento = "ZZ6914"
+semilla = semillas[1]
+experimento = "ZZ6916"
 
 # Create an empty DataFrame to store results
 results = pd.DataFrame(columns=["experimento", "semilla", "envio", "resultado"])
 
 
-for envio in range(9000, 13501, 500):
+for envio in range(9500, 12501, 500):
     if envio < 10000:
-        filename = f"{experimento}_01_061_s{semilla}_0{envio}.csv"
+        filename = f"{experimento}_01_028_s{semilla}_0{envio}.csv"
     if envio >= 10000:
-        filename = f"{experimento}_01_061_s{semilla}_{envio}.csv"
+        filename = f"{experimento}_01_028_s{semilla}_{envio}.csv"
     
     if not os.path.isfile(filename):
         print(f"No está el archivo {filename}. Revisar nombres de archivos.")
